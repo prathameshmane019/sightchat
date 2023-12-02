b50e684dade7e0be4121fbeac9bf4b49a966e907
prathameshmane019 <maneprathamesh019@gmail.com>
2023-12-02 20:54:34 +0530

    update 1.9

--- app/components/search.jsx
+++ app/components/search.jsx
@@ -4,7 +4,6 @@ import {Input} from "@nextui-org/react";
 export default function SearchBar() {
   return (
     <div  className=" w-90  md:flex-nowrap  mx-4 md:mb-0 ">
-    
     <Input type="search" variant='underlined' label="Search"  />
   </div>
     
