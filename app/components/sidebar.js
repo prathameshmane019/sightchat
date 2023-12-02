e23d54f3a2a84b10cd0ef13c9a33926667709049
prathameshmane019 <maneprathamesh019@gmail.com>
2023-12-02 23:06:24 +0530

    update 1.9.6

--- app/components/sidebar.js
+++ app/components/sidebar.js
@@ -8,13 +8,13 @@ import ProfileSkeleton from './skeletons/user'
 export default function Sidebar({loggedInUser}) {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true); // Added loading state
-
+const email =loggedInUser?.email
   useEffect(() => {
     const fetchUsers = async () => {
       try {
         const response = await fetch('/api/user');
         const data = await response.json();
-        setUsers(data.users.filter(user => user.email !== loggedInUser.email))
+        setUsers(data.users.filter(user => user?.email !== email))
         setLoading(false);
       } catch (error) {
         console.error('Error fetching users:', error);
@@ -22,7 +22,7 @@ export default function Sidebar({loggedInUser}) {
 
     };
     fetchUsers();
-  }, [loggedInUser?.email]);
+  }, [email]);
   return (
     <div className='w-[23vw] h-[100vh]  flex flex-col overflow-x-hidden overflow-y-auto bg-slate-100 dark:bg-neutral-900 '>
       <div className='text-xl h-[7vh] bg-secondary-700 flex items-center '>
